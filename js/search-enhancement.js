52a770f6a7e26cb54afe90fa8be50b5855f17f02
brianbatesd <brianbatesd@gmail.com>
2019-08-28 12:18:50 -0700

    Update file path for mobile search-crimson.png icon

--- js/search-enhancement.js
+++ js/search-enhancement.js
@@ -6,7 +6,7 @@ jQuery(document).ready(function($) {
   if (htmlWidth <= 739) {
   	/* swap hamburger for search icon */
     $('#nav-toggle').html(
-      "<img class='search-icon' src='https://vancouver.wsu.edu/sites/www.vancouver.wsu.edu/themes/omegawsuv/search-crimson.png' alt='Search'>"
+      "<img class='search-icon' src='https://www.vancouver.wsu.edu/themes/bootstrap_sass_wsuv/images/search-crimson.png' alt='Search'>"
     );
     /* set width of search bar (-34px for search icon) */
     $('#google-cse').find(".form-item>input").width((0.90 *
@@ -18,7 +18,7 @@ jQuery(document).ready(function($) {
     var htmlWidth = $('html').width();
     if (htmlWidth <= 739) {
     	$('#nav-toggle').html(
-      "<img class='search-icon' src='https://vancouver.wsu.edu/sites/www.vancouver.wsu.edu/themes/omegawsuv/search-crimson.png' alt='Search'>"
+      "<img class='search-icon' src='https://www.vancouver.wsu.edu/themes/bootstrap_sass_wsuv/images/search-crimson.png' alt='Search'>"
     );
       $('#google-cse').find(".form-item>input").width((0.90 *
           htmlWidth) -
